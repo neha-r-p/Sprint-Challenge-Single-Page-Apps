c69ec7b02c7f69e003ff52a54ee877611eef8bdb
Neha Patel <neha.r.p@gmail.com>
2019-07-26 10:42:38 -0700

    LocationCard.js: styled the cards with Semantic UI

--- src/components/LocationCard.js
+++ src/components/LocationCard.js
@@ -1,13 +1,20 @@
-import React from 'react'
+import React from "react";
+import { Card, Icon } from "semantic-ui-react";
 
-export default function LocationCard ({ name, type, dimension, residents }) {
+export default function LocationCard({ name, type, dimension, residents }) {
   // image={image}
   return (
-  <div>
-    <h2>{name}</h2>
-    <p>{type} - {dimension}</p>
-    <p>{residents.length} Residents</p>
-  </div>
-  
-  )
+    <Card>
+      <Card.Content>
+        <Card.Header>{name}</Card.Header>
+        <Card.Description>
+          {type} - {dimension}
+        </Card.Description>
+      </Card.Content>
+      <Card.Content extra>
+        <Icon name="users" />
+        {residents.length} Residents
+      </Card.Content>
+    </Card>
+  );
 }
